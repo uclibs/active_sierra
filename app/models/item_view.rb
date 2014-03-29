0f94e7cc8cae07851e8cb0ffaa29f3277ed0cb7c
James Van Mil <james.vanmil@gmail.com>
2014-03-29 15:58:20 -0400

    Add methods to item and order views to lookup bib view

--- app/models/item_view.rb
+++ app/models/item_view.rb
@@ -7,4 +7,15 @@ class ItemView < SierraTable
 	def item_record()
 		ItemRecord.find(self.id)
 	end
+
+	def first_bib_view()
+		# "first" because each item may have more than one linked bib record
+		self.item_record.bib_records[0].bib_view
+	end
+
+	def all_bib_views()
+		list = Array.new
+		self.item_record.bib_records.each { |b| list << b.bib_view }
+		list
+	end
 end
--- app/models/order_view.rb
+++ app/models/order_view.rb
@@ -7,4 +7,15 @@ class OrderView < SierraTable
 	def order_record()
 		OrderRecord.find(self.id)
 	end
+
+	def first_bib_view()
+		# "first" because each order may have more than one linked bib record
+		self.order_record.bib_records[0].bib_view
+	end
+
+	def all_bib_views()
+		list = Array.new
+		self.order_record.bib_records.each { |b| list << b.bib_view }
+		list
+	end
 end
