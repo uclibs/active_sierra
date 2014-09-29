0f5e582c556f23c65a08c44bcff687a1573c2363
Sean Crowe <crowesn@gmail.com>
2014-09-29 14:14:41 -0400

    Revert "add order record payment support"

--- app/models/order_record_paid.rb
+++
@@ -1,5 +0,0 @@
-class OrderRecordPaid < SierraTable
-
-  self.table_name = 'order_record_paid'
-  self.primary_key = 'id'
-end
--- app/models/order_view.rb
+++ app/models/order_view.rb
@@ -7,7 +7,6 @@ class OrderView < SierraTable
   has_many :item_records, through: :bib_record
   has_many :item_views, through: :item_records  
   has_one :record_metadata, foreign_key: 'id'
-  has_many :order_record_paids, foreign_key: 'order_record_id'
 
   self.table_name = 'order_view'
   self.primary_key = 'id'
